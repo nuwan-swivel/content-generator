a47e98cfde7f75391baf6c338ef2f9c9f22e1cb5
Nuwan Bandara <nbandara@tphglobal.com>
2023-09-14 18:55:42 +0530

    Refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import ImageGenerator from "./components/ImagesGenerator/ImageGenerator";
+import SpeechGenerator from "./components/SpeechGenerator/SpeechGenerator";
 import VideoScriptGenerator from "./components/VideoScriptGenerator/VideoScriptGenerator";
+
+enum PROMPTS {
+  VIDEO_SCRIPT = "VIDEO_SCRIPT",
+  IMAGE_GENERATION = "IMAGE_GENERATION",
+  AUDIO_GENERATION = "AUDIO_GENERATION",
+}
+
 function App() {
-  const [selectedScript, SetSelectedScript] = useState<string | undefined>(
-    undefined
-  );
-  return (
-    <div className="App">
-      {!selectedScript && (
-        <VideoScriptGenerator
-          onSelectScript={(script) => SetSelectedScript(script)}
-        />
-      )}
-      {selectedScript && <ImageGenerator selectedScript={selectedScript} />}
-    </div>
-  );
+  const [step, setStep] = useState<PROMPTS>(PROMPTS.VIDEO_SCRIPT);
+
+  const [videoScript, setVideoScript] = useState<string | undefined>(undefined);
+
+  function handleSelectVideoScript(script: string) {
+    setVideoScript(script);
+    setStep(PROMPTS.IMAGE_GENERATION);
+  }
+
+  function renderComponent() {
+    switch (step) {
+      case PROMPTS.VIDEO_SCRIPT:
+        return (
+          <VideoScriptGenerator onSelectScript={handleSelectVideoScript} />
+        );
+      case PROMPTS.IMAGE_GENERATION:
+        return <ImageGenerator selectedScript={videoScript || ""} />;
+      case PROMPTS.AUDIO_GENERATION:
+        return <SpeechGenerator />;
+      default:
+        return <div>Steps not supported</div>;
+    }
+  }
+
+  return <div className="container mx-auto">{renderComponent()}</div>;
 }
 
 export default App;
